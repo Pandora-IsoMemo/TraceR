fb9d5fb8da9fc5bf8ff0a9dcd3492a9eacc40139
Antonia Runge <antonia.runge@inwt-statistics.de>
2024-06-28 09:56:26 +0200

    Release version 24.06.1 (#18)
    * minimal example
    
    * Update inst/app/server.R
    
    Co-authored-by: Antonia Runge <antonia.runge@inwt-statistics.de>
    
    * Update inst/app/server.R
    
    Co-authored-by: Antonia Runge <antonia.runge@inwt-statistics.de>
    
    * Feature/11 export button (#15)
    
    * refactor
    
    * download and upload
    
    * remove old files
    
    * fix test
    
    ---------
    
    Co-authored-by: Lukas Fuchs <lukas.fuchs@inwt-statistics.de>
    Co-authored-by: Lukas Fuchs <86153843+f-lukas@users.noreply.github.com>

---
+++ inst/app/server.R
@@ -0,0 +1,31 @@
+shinyServer(function(input, output, session) {
+  # Reactive graph element that is updated regularly
+  graph <- reactiveVal()
+
+  # Create example graph after click on button
+  observe(graph(createExampleGraph())) %>% bindEvent(input$generate_flowchart)
+
+  # Automatically render the graph after updates
+  output$flowchart <- renderGrViz({
+    renderFlowchart(graph)
+  })
+
+  # Download and upload
+  downloadModuleServer("download_unsigned", graph=graph)
+  uploadedGraph <- importModuleServer("import")
+  updateGraph(graph=graph, uploadedGraph=uploadedGraph)
+
+  # Display clicked node id
+  displayNodeId(input, output, outputId="clickMessage")
+
+  # Custom js to add panzoom after the svg was created
+  # Does not work currently (anymore)
+  shinyjs::runjs('
+    setTimeout(function(){ // 500ms timeout as svg is not immediately available
+     var element = document.querySelector("#flowchart svg");
+     panzoom(element, {
+      bounds: true,
+      boundsPadding: 0.1
+    });
+}, 500);')
+})
---
+++ inst/app/ui.R
@@ -0,0 +1,36 @@
+library(shiny)
+library(DiagrammeR)
+
+tagList(
+  navbarPage(
+    title = paste("TraceR", packageVersion("TraceR")),
+    theme = shinythemes::shinytheme("flatly"),
+    position = "fixed-top",
+    collapsible = TRUE,
+    id = "tab",
+    tabPanel(
+      title = "Network",
+      sidebarLayout(
+        sidebarPanel(
+          width = 2,
+          actionButton("generate_flowchart", "Generate Flowchart"),
+          tags$br(),
+          tags$br(),
+          downloadModuleUI("download_unsigned"),
+          importModuleUI("import"),
+          tags$br(),
+          tags$br()
+        ),
+        mainPanel(
+          grVizOutput("flowchart", width = "600px", height = "600px"),
+          textOutput("clickMessage")
+        ),
+      )
+    )
+  ),
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "custom.css"),
+    tags$script(src = "https://unpkg.com/panzoom@9.4.0/dist/panzoom.min.js")
+  ),
+  shinyjs::useShinyjs()
+)
