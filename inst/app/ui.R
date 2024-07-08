40fcb0d562dc5f83b4e0b36bf652122f56e4c508
Antonia Runge <antonia.runge@inwt-statistics.de>
2024-07-08 14:55:17 +0200

    update description

--- inst/app/ui.R
+++ inst/app/ui.R
@@ -15,6 +15,8 @@ tagList(
           width = 2,
           actionButton("generate_flowchart", "Generate Flowchart"),
           tags$br(),
+          # this is only a placeholder for actual inputs,
+          # can be removed whe we have more inputs that can be stored in a session download:
           textInput("test_input", "Test Input", placeholder = "Please add some text ..."),
           tags$br(),
           downloadModuleUI("download_unsigned"),
