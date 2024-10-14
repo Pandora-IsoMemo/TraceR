b55ac1a6d67bd713a637a50ea4b147a8db179cf2
Jan Abel <jan.abel@office-platform.net>
2024-10-14 17:48:17 +0200

    add test - check pem files

---
+++ tests/testthat/key-exists.R
@@ -0,0 +1,9 @@
+test_that("private_key.pem exists in inst/app", {
+  file_path <- "inst/app/private_key.pem"
+  expect_true(file.exists(file_path), info = "The file private_key.pem should exist in inst/app")
+})
+
+test_that("public_key.pem exists in inst/app", {
+  file_path <- "inst/app/public_key.pem"
+  expect_true(file.exists(file_path), info = "The file private_key.pem should exist in inst/app")
+})
